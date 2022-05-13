2cb39d11724d581574ed39073a538a456a09c52f
shrikewale <shrikewale@shrikants-air.lan>
2022-05-13 16:50:50 -0400

    Committing second change as per lead

--- src/gitDemo/Test2fromMac.java
+++ src/gitDemo/Test2fromMac.java
@@ -5,6 +5,7 @@ public class Test2fromMac {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello World2 from MaC ");
+System.out.print("Changes made by SKgithub2 as per suggestion from lead Uno-Shri");
 
 }
 }
